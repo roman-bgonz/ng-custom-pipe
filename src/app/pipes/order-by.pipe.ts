f5dba99bc7215e2908d2606b1642209eba0a80d5
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2023-03-28 16:21:33 -0600

    Order by id and css by selection order

--- src/app/pipes/order-by.pipe.ts
+++ src/app/pipes/order-by.pipe.ts
@@ -13,8 +13,23 @@ export class OrderByPipe implements PipeTransform {
     const [property, order = 'asc']: string[] = args;
 
     return value.sort((a, b) => {
-      const compare = a[property].localeCompare(b[property]);
-      return order == 'asc' ? compare : -compare;
+      if (typeof a[property] == 'string') {
+        const compare = a[property].localeCompare(b[property]);
+        return order == 'asc' ? compare : -compare;
+      } else {
+        const orderNum = this.orderNumberFn(a, b, property);
+        return order == 'asc' ? orderNum : -orderNum;
+      }
     });
   }
+
+  private orderNumberFn = (a: any, b: any, property: string) => {
+    if (a[property] > b[property]) {
+      return 1;
+    } else if (a[property] < b[property]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
 }
